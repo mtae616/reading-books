a6f6be3726517fcec22885e3fa68326938496d96
강태수 <mtae616@naver.com>
2022-09-08 11:59:47 +0900

    add 19, 23

--- 이코테/subjects_sort/23_국영수/23_국영수2.py
+++ 이코테/subjects_sort/23_국영수/23_국영수2.py
@@ -1,1 +1,10 @@
 # https://www.acmicpc.net/problem/10825
+lst = []
+
+for i in range(int(input())):
+    name, ko, en, ma = input().split()
+    lst.append((name, int(ko), int(en), int(ma)))
+
+lst.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
+for l in lst:
+    print(l[0])
