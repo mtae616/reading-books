6b06d8f2bd6776fa041341856ee865597c6d9b19
Taesu Kang <mtae616@naver.com>
2022-10-21 13:03:20 +0900

    add 18

--- 이코테/subjects_DFS_BFS/18_괄호변환/18_괄호변환2.py
+++ 이코테/subjects_DFS_BFS/18_괄호변환/18_괄호변환2.py
@@ -1,5 +1,52 @@
-def solution():
-	
-	return
+def check(p):
+	stack = []
 
-solution("(()())()")
+	i = 0
+	while i < len(p):
+		if p[i] == '(':
+			stack.append('(')
+		elif p[i] == ')':
+			if len(stack) == 0:
+				return False
+			temp = stack.pop()
+			if temp != '(':
+				return False
+		i += 1
+	return True
+
+def split(p):
+	left_cnt = 0
+	right_cnt = 0
+
+	i = 0
+	while i < len(p):
+		if left_cnt != 0 and right_cnt != 0:
+			if left_cnt == right_cnt:
+				break
+		if p[i] == '(':
+			left_cnt += 1
+		elif p[i] == ')':
+			right_cnt += 1
+		i += 1
+	return p[:i], p[i:]
+
+def solution(u):
+	if not u:
+		return ""
+	temp = ""
+	u, v = split(u)
+	if check(u):
+		return u + solution(v)
+	elif not check(u):
+		temp += "("
+		temp += solution(v)
+		temp += ")"
+		for i in u[1:len(u) - 1]:
+			if i == '(':
+				temp += ')'
+			else:
+				temp += '('
+	return temp
+
+
+print(solution("()))((()"))
