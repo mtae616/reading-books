3002ff6cb0e17720ccae91d16e5394217ebc714a
Tae-Su Kang <mtae616@naver.com>
2023-03-16 22:38:53 +0900

    add 30, 33

---
+++ cote-java/ch09-hash/src/ch09_33.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+
+    public static double getSlope(int x1, int y1, int x2, int y2) {
+        return (double) (y2 - y1) / (x2 - x1);
+    }
+
+    public static int solution(int[][] dots) {
+        Set<Double> slopes = new HashSet<>();
+
+        for (int i = 0; i < dots.length; i++) {
+            for (int j = i + 1; j < dots.length; j++) {
+                double slope = getSlope(dots[i][0], dots[i][1], dots[j][0], dots[j][1]);
+                if (slopes.contains(slope)) return 1;
+                slopes.add(slope);
+            }
+        }
+
+        return 0;
+    }
+}
+
+public class ch09_33 {
+    public static void main(String[] args) {
+        Solution.solution(new int[][]{
+                {1, 4},
+                {9, 2},
+                {3, 8},
+                {11, 6}
+        });
+    }
+}
