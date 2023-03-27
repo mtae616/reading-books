167d39b9e018aa7fef167b8f6dd3f3cd0b3d97a1
Tae-Su Kang <mtae616@naver.com>
2023-03-27 22:24:49 +0900

    add 17, 20

--- cote-java/ch06-ex-search/src/ch06_20.java
+++ cote-java/ch06-ex-search/src/ch06_20.java
@@ -1,8 +1,50 @@
+import java.util.*;
+
 public class ch06_20 {
     static class Solution {
-        public static long solution(String expression) {
-            long answer = 0;
+        public static String[][] arr = {
+                {"+", "-", "*"},
+                {"+", "*", "-"},
+                {"-", "+", "*"},
+                {"-", "*", "+"},
+                {"*", "+", "-"},
+                {"*", "-", "+"}
+        };
 
+        public static long cal(List<String> tokenList, String[] opList) {
+            int idx = 0;
+            while (idx < 3) {
+                for (int i = 0; i < tokenList.size(); i++) {
+                    if (tokenList.get(i).equals(opList[idx])) {
+                        long a = Long.parseLong(tokenList.get(i - 1));
+                        long b = Long.parseLong(tokenList.get(i + 1));
+
+                        String op = opList[idx];
+                        long temp;
+                        if (op.equals("+")) temp = a + b;
+                        else if (op.equals("-")) temp = a - b;
+                        else temp = a * b;
+
+                        tokenList.remove(i - 1);
+                        tokenList.remove(i - 1);
+                        tokenList.remove(i - 1);
+                        tokenList.add(i - 1, String.valueOf(temp));
+                        i -= 2;
+                    }
+                }
+                idx += 1;
+            }
+            return Long.parseLong(tokenList.get(0));
+        }
+
+        public static long solution(String expression) {
+            long answer = (long) -(1e9);
+            StringTokenizer st = new StringTokenizer(expression, "+-*", true);
+            ArrayList<String> tokenList = new ArrayList<>();
+
+            while (st.hasMoreTokens()) { tokenList.add(st.nextToken()); }
+
+            for (int i = 0; i < arr.length; i++) answer = Math.max(answer, Math.abs(cal(new ArrayList<>(tokenList), arr[i])));
             return answer;
         }
     }
